66627614c30f623d4fa7a3539380f620d14ece1e
KostasMp <mpenos.ks@gmail.com>
2016-03-30 20:10:58 +0300

    Fix #11

--- portscanner.py
+++ portscanner.py
@@ -5,11 +5,12 @@ import sys
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description = "Test for open ports...")
+    parser = argparse.ArgumentParser(description = "Test a specified IP for open ports.")
     mutex = parser.add_mutually_exclusive_group()
     parser.add_argument('-v', '--verbose', help='Add extra verbosity to the output of the scanner', action='store_true')
     mutex.add_argument('-a', '--all', help='Scan all the possible ports', action='store_true')
-    mutex.add_argument('-p', '--ports', help='Scan the specified ports only', type=int, choices=range(0,65536), nargs='*', default=[])
+    mutex.add_argument('-p', '--ports', help='Scan the specified ports only', type=int, metavar='PORT', choices=range(0,65536), nargs='*', default=[])
+
     args = parser.parse_args()
 
     if args.all:
