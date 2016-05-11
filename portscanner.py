b90927f3aa8b3178eabdf75ab68cf11df1c08835
KostasMp <mpenos.ks@gmail.com>
2016-05-11 22:51:20 +0300

    Resolve #8 and modify output

--- portscanner.py
+++ portscanner.py
@@ -5,6 +5,15 @@ import sys
 import os
 import argparse
 
+class colors:
+	HOST='\033[32m'
+	PORT='\033[36m'
+	TYPE='\033[33m'
+	SERVICE='\033[35m'
+	WARNING='\033[31m'
+	INFO='\033[34m'
+	END='\033[0m'
+
 def main():
 	parser = argparse.ArgumentParser(description = "Test a specified ip/host for open ports.")
 	mutex = parser.add_mutually_exclusive_group(required=True)
@@ -25,14 +34,14 @@ def main():
 	try:
 		ip = socket.gethostbyname(args.ip)
 	except socket.gaierror:
-		sys.exit('[!] Invalid ip-address/hostname!\n[!] Exiting...')
+		sys.exit(colors.WARNING + '[!] ' + colors.END + 'Invalid ip-address/hostname!\n' + colors.WARNING + '[!] ' + colors.END + 'Exiting...')
 	try:
 		host = socket.gethostbyaddr(ip)[0]
 	except socket.herror:
 		host = 'Uknown Host'
 	protocol = args.type
 
-	print '[+] Pinging host ' + ip + ":" + host
+	print colors.INFO + '[+] ' + colors.END + 'Pinging host ' + ip + ":" + host
 	os.system('ping -q -c1 -w2 ' + ip)
 	print ""
 
@@ -49,7 +58,7 @@ def main():
 			serv = 'Uknown Service'
 		try:
 			if args.verbose:
-				print '[+] Attempting to connect to ' + ip + '(' + host + ')' + ':' + str(port) + '/' + protocol.upper() + ' [' + serv.upper() + ']'
+				print colors.INFO + '[+] ' + colors.END + 'Attempting to connect to ' + colors.INFO + '::' + colors.END + colors.HOST + ip + colors.END + colors.INFO + ':' + colors.END + colors.HOST +  host + colors.END + colors.INFO + '::' + colors.END + ' via port ' + colors.PORT + str(port) + colors.END + '/' + colors.TYPE + protocol.upper() + colors.END + ' [' + colors.SERVICE + serv.upper() + colors.END + ']'
 			s.connect((ip ,int(port)))
 			s.send("Port Checking")
 			try:
@@ -60,7 +69,7 @@ def main():
 			continue		    # hence the port is closed... In that case advance to the next port.
 		if banner=='':
 			banner = 'No Response...'
-		print '[+] Port ' + str(port) + '/' + protocol.upper() + ' [' + serv.upper() + ']' + ' is open!' + '  ==> Reply: ' + str(banner)
+		print colors.INFO + '[+] ' + colors.END + 'Port ' + colors.PORT + str(port) + colors.END + '/' + colors.TYPE + protocol.upper() + colors.END + ' [' + colors.SERVICE +serv.upper() + colors.END + ']' + ' is open!' + '  ' + colors.INFO + '==>' + colors.END + ' Reply: ' + str(banner)
 		s.close()
 
 if __name__ =='__main__':
