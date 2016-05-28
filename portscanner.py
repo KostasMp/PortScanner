31c1a97f27cf93def7ba87f3d9d0bafd300ccffc
KostasMp <mpenos.ks@gmail.com>
2016-05-29 01:08:59 +0300

    Modify code to support multiple host scanning

--- portscanner.py
+++ portscanner.py
@@ -10,7 +10,7 @@ from src.loadbar import loading_bar
 def main():
 	parser = argparse.ArgumentParser(description = "Test a specified ip/host for open ports.")
 	mutex = parser.add_mutually_exclusive_group(required=True)
-	parser.add_argument('ip', metavar='IP', help='The ip/host to be scanned for open ports')
+	parser.add_argument('hosts', metavar='HOST', help='The ip/host to be scanned for open ports', nargs='*')
 	parser.add_argument('-v', '--verbose', help='Add extra verbosity to the output of the scanner', action='store_true')
 	parser.add_argument('-t', '--type', help='The type of the ports to be scanned', choices=['tcp', 'TCP', 'udp', 'UDP'], metavar='[ udp | tcp ]', default='tcp')
 	mutex.add_argument('-a', '--all', help='Scan all the possible ports', action='store_true')
@@ -23,43 +23,46 @@ def main():
 		ports = range(0, 65536)
 	elif args.ports:
 		ports = args.ports
-	host = Host(args.ip)
+	
+	hosts = args.hosts
+	for target in hosts:
 
-	ping(host.ip, host.name)	
+		host = Host(target)
+		ping(host.ip, host.name)	
 
-	open_ports_meta = []
-	port_count = 0;
-	for port_num in ports:
-		port = Port(port_num, args.type.lower())
-		if (port.protocol == 'tcp'):								#
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # Protocol
-		elif (port.protocol == 'udp'):                          	# Checking
-			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    #
-		service = Service(port)
-		banner = False
-		s.settimeout(3)
+		open_ports_meta = []
+		port_count = 0;
+		for port_num in ports:
+			port = Port(port_num, args.type.lower())
+			if (port.protocol == 'tcp'):								#
+				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # Protocol
+			elif (port.protocol == 'udp'):                          	# Checking
+				s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    #
+			service = Service(port)
+			banner = False
+			s.settimeout(3)
 
-		port_count += 1
-		loading_bar(port_count, len(ports), True)
+			port_count += 1
+			loading_bar(port_count, len(ports), True)
 		
-		try:
-			if args.verbose:
-				verbose_message(host.ip, host.name, port.num, port.protocol, service.name)
-			s.connect((host.ip, port.num))	# Connect to an ip using the specified port
-			s.send("Port Checking")		# Send the message 'Port Checking' to the connected server
 			try:
-				banner = s.recv(1024)		# Attempt to get a response message
-			except socket.timeout:
-				banner = ''
-		except socket.error:        # If a socket.error exception is caught the attempt to connect has failed, 
-			continue                # hence the port is closed. In that case advance to the next port
-		if banner=='':
-			banner = 'No Response...'
-		open_ports_meta.append((port, service, banner))
-		s.close()
+				if args.verbose:
+					verbose_message(host.ip, host.name, port.num, port.protocol, service.name)
+				s.connect((host.ip, port.num))	# Connect to an ip using the specified port
+				s.send("Port Checking")		# Send the message 'Port Checking' to the connected server
+				try:
+					banner = s.recv(1024)		# Attempt to get a response message
+				except socket.timeout:
+					banner = ''
+			except socket.error:        # If a socket.error exception is caught the attempt to connect has failed, 
+				continue                # hence the port is closed. In that case advance to the next port
+			if banner=='':
+				banner = 'No Response...'
+			open_ports_meta.append((port, service, banner))
+			s.close()
 
-	for item in open_ports_meta:
-		results_message(item[0].num, item[0].protocol, item[1].name, item[2])
+		for item in open_ports_meta:
+			results_message(item[0].num, item[0].protocol, item[1].name, item[2])
 
 def verbose_message(ip, hostname, port_num, port_protocol, service_name):   # The extra message printed if the verbose option is on
 	print Colors.INFO + '[+] ' + Colors.END + 'Attempting to connect to ' + Colors.INFO + '::' + Colors.END + Colors.HOST + ip + Colors.END + Colors.INFO + ':' + Colors.END + Colors.HOST + hostname + Colors.END + Colors.INFO + '::' + Colors.END + ' via port ' + Colors.PORT + str(port_num) + Colors.END + '/' + Colors.TYPE + port_protocol + Colors.END + ' [' + Colors.SERVICE + service_name.upper() + Colors.END + ']'
